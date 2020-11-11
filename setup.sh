9f1cf99e291be734e63743b7f3f718a210543c3b
Saturn Karry <skarry@si-f5.kzn.21-school.ru>
2020-11-11 20:17:40 +0300

    all

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 #all_restart
 # minikube stop
-# minikube delete 
+minikube delete 
 
 
 # start
@@ -19,11 +19,22 @@ minikube addons enable metallb
 
 
 # proces
+kubectl apply -f ./configmap.yaml
+kubectl apply -f ./volume.yaml
 docker build -t nginx_image ./nginx/
 docker build -t ftps_image ./ftps/
-kubectl apply -f ./configmap.yaml 
+docker build -t mysql_image ./mysql/
+docker build -t phpmyadmin_image ./phpmyadmin/
+docker build -t wordpress_image ./wordpress/
+docker build -t influx_image ./influx/
+docker build -t grafana_image ./grafana/
 kubectl apply -f ./nginx/nginx.yaml
 kubectl apply -f ./ftps/ftps.yaml
+kubectl apply -f ./mysql/mysql.yaml
+kubectl apply -f ./phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./wordpress/wordpress.yaml
+kubectl apply -f ./influx/influx.yaml
+kubectl apply -f ./grafana/grafana.yaml
 
 
 # check
