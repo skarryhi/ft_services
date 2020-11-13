0bf1292fa212ca595621b8b83d1023b7494b6e1d
Saturn Karry <skarry@si-f5.kzn.21-school.ru>
2020-11-13 17:55:23 +0300

    all

--- setup.sh
+++ setup.sh
@@ -1,49 +1,31 @@
 #!bin/sh
 
-# mini_restart
-# kubectl delete deploy nginx-deployment
-# kubectl delete svc nginx-svc
-
-
-#all_restart
-# minikube stop
-minikube delete 
-
-
-# start
-# minikube start --vm-driver=virtualbox
 minikube start --vm-driver=virtualbox || exit 1
 eval $(minikube docker-env)
 minikube addons enable metallb
-# minikube addons enable dashboard
-
 
 # proces
-kubectl apply -f ./configmap.yaml
-kubectl apply -f ./volume.yaml
-docker build -t nginx_image ./nginx/
-docker build -t ftps_image ./ftps/
-docker build -t mysql_image ./mysql/
-docker build -t phpmyadmin_image ./phpmyadmin/
-docker build -t wordpress_image ./wordpress/
-docker build -t influx_image ./influx/
-docker build -t grafana_image ./grafana/
-kubectl apply -f ./nginx/nginx.yaml
-kubectl apply -f ./ftps/ftps.yaml
-kubectl apply -f ./mysql/mysql.yaml
-kubectl apply -f ./phpmyadmin/phpmyadmin.yaml
-kubectl apply -f ./wordpress/wordpress.yaml
-kubectl apply -f ./influx/influx.yaml
-kubectl apply -f ./grafana/grafana.yaml
+kubectl apply -f ./srcs/configmap.yaml
+kubectl apply -f ./srcs/volume.yaml
 
 
-# check
-# kubectl get deploy 
-# kubectl get svc
+# docker
+docker build -t nginx_image ./srcs/nginx/
+docker build -t ftps_image ./srcs/ftps/
+docker build -t mysql_image ./srcs/mysql/
+docker build -t phpmyadmin_image ./srcs/phpmyadmin/
+docker build -t wordpress_image ./srcs/wordpress/
+docker build -t influx_image ./srcs/influx/
+docker build -t grafana_image ./srcs/grafana/
+
+
+kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/ftps/ftps.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/influx/influx.yaml
+kubectl apply -f ./srcs/grafana/grafana.yaml
+
+
 # minikube dashboard
-
-# sh
-# sh
-
-
-
